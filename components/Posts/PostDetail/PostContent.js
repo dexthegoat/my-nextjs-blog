fdb6944297f9ef583ca169d92739dbf7cd2f4b66
Jianshuo Yang <jsy.bornconfused@gmail.com>
2022-06-11 08:02:00 -0400

    added customized react markdown, contact form

--- components/Posts/PostDetail/PostContent.js
+++ components/Posts/PostDetail/PostContent.js
@@ -1,16 +1,48 @@
 import ReactMarkdown from 'react-markdown';
 import PostHeader from './PostHeader';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import classes from './PostContent.module.css';
 
 export default function PostContent({ post }) {
-  const path = `/images/posts/${post.slug}/${post.image}`,
-    title = post.title;
+  const path = `/images/posts/${post.slug}/${post.image}`;
+  const title = post.title;
+
+  const customRenderers = {
+    p({ node, children }) {
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+    code({ className, children }) {
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={path} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
